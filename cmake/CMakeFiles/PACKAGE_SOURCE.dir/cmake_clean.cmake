b878236524e3267a3fe4b935c8ba372738debed0
Mike Estee <mikest@gmail.com>
2013-10-29 07:47:47 -0700

    before cmake

--- cmake/CMakeFiles/PACKAGE_SOURCE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/PACKAGE_SOURCE"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/PACKAGE_SOURCE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
